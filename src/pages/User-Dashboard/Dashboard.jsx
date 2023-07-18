f7dbd0ff7510c9b83d7465df554de98cda9ea17a
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-18 17:08:36 +0100

    Added the routes to the various user dashboard pages

--- src/pages/User-Dashboard/Dashboard.jsx
+++ src/pages/User-Dashboard/Dashboard.jsx
@@ -1,43 +1,65 @@
 import { Box } from "@mui/material";
-import styles from './dashboard.module.css'
-import AdminHeader from "../../components/AdminHeader";
+import styles from "./dashboard.module.css";
+import AdminHeader from "../../userScenes/userGlobal/AdminHeader";
 import DashContent from "../../components/DashContent";
-import Searchbar from "../../components/Searchbar";
-import Sidebar from "../../components/Sidebar";
+import Searchbar from "../../userScenes/userGlobal/Searchbar";
+import Sidebar from "../../userScenes/userGlobal/Sidebar";
 import { connect } from "react-redux";
 import { setToggleView } from "../../redux/navToggle/navToggleAction";
 import { selectToggleView } from "../../redux/navToggle/navToggleSelect";
 import { createStructuredSelector } from "reselect";
 import MobileView from "../../components/MobileView";
 
-import {useMediaQuery} from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
+import { useMediaQuery } from "@mui/material";
+import Deposits from "../../userScenes/Deposit/Deposits";
+import Transactions from "../../userScenes/Transactions/Transactions";
+import Referral from "../../userScenes/Referral/Referral";
+import Withdrawal from "../../userScenes/Withdrawal/Withdrawal";
+import SubscriptionPlan from "../../userScenes/Subscription-plan/SubscriptionPlan";
+import AccountSettings from "../../userScenes/Account-settings/AccountSettings";
+import TwoFactorAuth from "../../userScenes/TwoFactorAuth/TwoFactorAuth";
+import MyMatrix from "../../userScenes/MyMatrix/MyMatrix";
+import TransferBalance from "../../userScenes/Transfer-balance/TransferBalance";
+import EpinRecharge from "../../userScenes/Epin-recharge/EpinRecharge";
 
-const Dashboard = ({collapsed, setIsCollapsed}) => {
-  const isNotMobile = useMediaQuery('(min-width: 600px)')
+const Dashboard = ({ collapsed, setIsCollapsed }) => {
+  const isNotMobile = useMediaQuery("(min-width: 600px)");
   return (
     <div className={styles.dashboard}>
       {!isNotMobile && <MobileView />}
-      <Box className={`${collapsed ? styles.hide : styles.show} ${styles.sidebar}`} flex={isNotMobile ? 1 : 0}>
+      <Box
+        className={`${collapsed ? styles.hide : styles.show} ${styles.sidebar}`}
+        flex={isNotMobile ? 1 : 0}
+      >
         <Sidebar />
-        
       </Box>
       <div className={styles.topbar}>
         <div className={styles.background}></div>
         <Searchbar />
-        <Box display="flex" justifyContent="space-between" alignItems="center">
-          <AdminHeader title="Dashboard" subtitle="Welcome to your dashboard" />
-        </Box>
 
-        <DashContent />
+        <Routes>
+          <Route path="dashboard" element={<DashContent />} />
+          <Route path="deposit" element={<Deposits />} />
+          <Route path="transactions" element={<Transactions />} />
+          <Route path="referral" element={<Referral />} />
+          <Route path="withdrawal" element={<Withdrawal />} />
+          <Route path="subscription" element={<SubscriptionPlan />} />
+          <Route path="account-settings" element={<AccountSettings />} />
+          <Route path="2fa-security" element={<TwoFactorAuth />} />
+          <Route path="mymatrix" element={<MyMatrix />} />
+          <Route path="transfer-balance" element={<TransferBalance />} />
+          <Route path="epin-recharge" element={<EpinRecharge />} />
+        </Routes>
       </div>
     </div>
   );
 };
 const mapStateToProps = createStructuredSelector({
-  collapsed: selectToggleView
-})
-const mapDispatchToProps = dispatch => ({
-  setIsCollapsed: () => dispatch(setToggleView())
-})
+  collapsed: selectToggleView,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setIsCollapsed: () => dispatch(setToggleView()),
+});
 export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
