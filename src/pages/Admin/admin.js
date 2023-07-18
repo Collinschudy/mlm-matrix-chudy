41a3cfe3476f91cdd06849b3473f7b0edd487c98
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-18 17:02:52 +0100

    Added the various routes for the different user dashboard pages

--- src/pages/Admin/admin.js
+++ src/pages/Admin/admin.js
@@ -2,29 +2,26 @@ import Topbar from "../../scenes/global/Topbar";
 import Sidebar from "../../scenes/global/Sidebar";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../../scenes/dashboard/dashboard";
-import Users from '../../scenes/users/Users';
+import Users from "../../scenes/users/Users";
 import Line from "../../scenes/Line";
 import Calendar from "../../scenes/calender/calender";
 
 const Admin = () => {
   return (
     <div className="content">
-         <Sidebar />
-        <div className="admin">
+      <Sidebar />
+      <div className="admin">
         <Topbar />
-        
-        
+
         <Routes>
-            <Route path='' element={<Dashboard />}/>
-            <Route path='users' element={<Users />}/>
-            <Route path='lines' element={<Line />}/>
-            <Route path='calendar' element={<Calendar />}/>
-
+          <Route path="" element={<Dashboard />} />
+          <Route path="users" element={<Users />} />
+          <Route path="lines" element={<Line />} />
+          <Route path="calendar" element={<Calendar />} />
         </Routes>
-        </div>
-        
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Admin
+export default Admin;
