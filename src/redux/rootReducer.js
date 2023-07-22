014d77262228380e04b14657ff3597585303c183
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-22 12:03:12 +0100

    updated the rootreducer to include the new redux state

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import toggleReducer from "./navToggle/navToggleReducer";
 import mobileReducer from './mobileToggle/mobileReducer';
+import userTokenAndEmailReducer from "./userInfo/userReducer";
 
 export default combineReducers({
     toggleView: toggleReducer,
-    mobileView: mobileReducer
+    mobileView: mobileReducer,
+    userTokenAndEmail: userTokenAndEmailReducer,
 })
