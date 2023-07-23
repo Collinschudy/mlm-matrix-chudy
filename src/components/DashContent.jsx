9b3cd5177141fb4b91f432ddec39d3259bfc1716
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-23 23:49:23 +0100

    Added the selectCurrentUser to get the user data and update the ui

--- src/components/DashContent.jsx
+++ src/components/DashContent.jsx
@@ -29,203 +29,221 @@ import { selectToggleView } from "../redux/navToggle/navToggleSelect";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import AdminHeader from "../userScenes/userGlobal/AdminHeader";
+import { selectCurrentUser } from "../redux/userInfo/userSelect";
+import { setCurrentUser } from "../redux/userInfo/userInfoAction";
 
-const DashContent = ({ collapsed, setIsCollapsed }) => {
+const DashContent = ({ collapsed, setIsCollapsed, userData, setUserData }) => {
   const isNotMobile = useMediaQuery("(min-width: 600px)");
 
   return (
     <>
-    <Box display="flex" justifyContent="space-between" alignItems="center">
-    <AdminHeader title="Dashboard" subtitle="Welcome to your dashboard" />
-  </Box>
-    <div>
-      <Box
-        m="20px"
-        backgroundColor="transparent"
-        borderRadius="5px"
-        
-        p={!isNotMobile ? "0" : "20px"}
-      >
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <AdminHeader title="Dashboard" subtitle={`Welcome to your dashboard, ${userData?.username} `} />
+      </Box>
+      <div>
         <Box
-          display="grid"
-          gridTemplateColumns="repeat(12, 1fr)"
-          gridAutoRows="180px"
-          gap="20px"
-          sx={{
-            "& > div": { gridColumn: isNotMobile ? undefined : "span 12" },
-          }}
+          m="20px"
+          backgroundColor="transparent"
+          borderRadius="5px"
+          p={!isNotMobile ? "0" : "20px"}
         >
           <Box
-            gridColumn="span 4"
-            background={``}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
+            display="grid"
+            gridTemplateColumns="repeat(12, 1fr)"
+            gridAutoRows="180px"
+            gap="20px"
             sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
+              "& > div": { gridColumn: isNotMobile ? undefined : "span 12" },
             }}
           >
-            <StatBox
-              icon={<MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Current Balance"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)'
+            <Box
+              gridColumn="span 4"
+              background={``}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title={`\u20A6${userData?.balance}`}
+                subtitle="Current Balance"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
 
-              // '#8ec33e'
+                // '#8ec33e'
                 // "linear-gradient(to bottom right, rgba(0,0,50), yellowgreen)",
-            }}
-          >
-            <StatBox
-              icon={<LocalAtmIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Deposits"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<PaymentIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Withdrawal"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Referral Commission"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Level Commission"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<ReceiptIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total E-Pin Recharged"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={
-                <AddCircleOutlinedIcon sx={{ height: "4em", width: "4em" }} />
-              }
-              title="N0"
-              subtitle="Total E-Pin Generated"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<SendIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Transferred Balance"
-            />
-          </Box>
-          <Box
-            gridColumn="span 4"
-            backgroundColor="grey"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            borderRadius="8px"
-            sx={{
-              background:'linear-gradient(to bottom right, #14213d, #8ec33e)',
-            }}
-          >
-            <StatBox
-              icon={<PersonAddIcon sx={{ height: "4em", width: "4em" }} />}
-              title="N0"
-              subtitle="Total Direct Referrals"
-            />
+              }}
+            >
+              <StatBox
+                icon={<LocalAtmIcon sx={{ height: "4em", width: "4em" }} />}
+                title='0'
+                subtitle="Total Deposits"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<PaymentIcon sx={{ height: "4em", width: "4em" }} />}
+                title={"\u20A6"}
+                subtitle="Total Withdrawal"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title={`"\u20A6"`}
+                subtitle="Total Referral Commission"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title="0"
+                subtitle="Total Level Commission"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<ReceiptIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total E-Pin Recharged"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <AddCircleOutlinedIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title="0"
+                subtitle="Total E-Pin Generated"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<SendIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total Transferred Balance"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<PersonAddIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total Direct Referrals"
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </div>
+      </div>
     </>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
   collapsed: selectToggleView,
+  userData: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   setIsCollapsed: () => dispatch(setToggleView()),
+  setUserData: (userData) => dispatch(setCurrentUser(userData)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashContent);
