338ada4697bcb14993a099f6bbd1458f5983d722
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-29 16:36:53 +0100

    removed dumb lines of html

--- src/userScenes/Deposit/Deposits.jsx
+++ src/userScenes/Deposit/Deposits.jsx
@@ -1,16 +1,14 @@
 import { Box } from '@mui/material'
 import React from 'react'
 import AdminHeader from '../userGlobal/AdminHeader';
+import styles from './deposits.module.css'
 
 const Deposits = () => {
   return (
     <>
     <AdminHeader title="Deposits" subtitle="Welcome to your deposits section" />
-    <div>
-      <Box>Deposits
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tempore ipsa molestias dicta \
-      aperiam est ipsam facilis recusandae, tempora itaque iusto blanditiis? Doloribus molestiae odio voluptate 
-      minus nam? Nam, nostrum?</Box>
+    <div className={styles.deposits}>
+    
     </div>
     </>
   )
