da9d923528c9ab711587e02c7794b19b637f0cfa
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-08-09 16:22:45 +0100

    Created the deposit page and its stylesheet

--- src/userScenes/Deposit/Deposits.jsx
+++ src/userScenes/Deposit/Deposits.jsx
@@ -1,17 +1,110 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import AdminHeader from '../userGlobal/AdminHeader';
-import styles from './deposits.module.css'
+import { useState, useRef } from "react";
 
-const Deposits = () => {
+import AdminHeader from "../userGlobal/AdminHeader";
+import styles from "./deposits.module.css";
+
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
+
+import { connect } from "react-redux";
+import {
+  selectCurrentUser,
+  selectUserTokenAndEmail,
+} from "../../redux/userInfo/userSelect";
+import {
+  setCurrentUser,
+  setUserTokenAndEmail,
+} from "../../redux/userInfo/userInfoAction";
+import { createStructuredSelector } from "reselect";
+
+const Deposits = ({ userData, userVerify }) => {
+  const [txn_ref, setTxn_ref] = useState("");
+  const form = useRef();
+
+  const generateRef = () => {
+    const timestamp = Date.now();
+    const randStr = uuidv4();
+    let reference = randStr;
+    return reference;
+  };
+
+  const initiateTransaction = async (e) => {
+    e.preventDefault();
+    const { token } = userVerify;
+
+    try {
+      const reference = generateRef();
+      setTxn_ref(reference);
+      const url =
+        "https://mlm.zurupevarietiesstore.com/api/transactions/initialize";
+      const body = {
+        email: userData.email,
+        amount: 1000,
+        callback_url: "http://localhost:3000/verifypayment",
+        phone: userData.phone_number,
+        reference: reference,
+      };
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      const res = await axios.post(url, body, { headers });
+      const authorizationUrl = res.data.data.transaction.authorization_url;
+      window.location.href = authorizationUrl;
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-    <AdminHeader title="Deposits" subtitle="Welcome to your deposits section" />
-    <div className={styles.deposits}>
-    
-    </div>
-    </>
-  )
-}
+      <AdminHeader
+        title="Deposits"
+        subtitle="Welcome to your deposits section"
+      />
+      <div className={styles.deposits}>
+        <div className={styles.depositbox}>
+          Click here to initiate deposit
+          <form onSubmit={initiateTransaction} ref={form}>
+            <div>
+              <label>Email</label>
+              <input type="text" defaultValue={userData.email} disabled />
+            </div>
+            <div>
+              <label>Phone</label>
+              <input
+                type="text"
+                defaultValue={userData.phone_number}
+                disabled
+              />
+            </div>
 
-export default Deposits;
+            <div>
+              <label>Amount</label>
+              <input type="text" defaultValue="1000" disabled />
+            </div>
+
+            <div>
+              <label>Reference</label>
+              <input type="text" defaultValue={txn_ref && txn_ref} disabled />
+            </div>
+            <div>
+              <button className={styles.button} type="submit">
+                Deposit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  userData: selectCurrentUser,
+  userVerify: selectUserTokenAndEmail,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setUserVerify: (user) => dispatch(setUserTokenAndEmail(user)),
+  setUserData: (userData) => dispatch(setCurrentUser(userData)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Deposits);
