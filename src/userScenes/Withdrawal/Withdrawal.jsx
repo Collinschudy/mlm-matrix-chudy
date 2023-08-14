e30a6e81a74b44de20a4c74cebcc30e6d836e989
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-08-14 16:01:02 +0100

    updated the page with the api requests and created its corresponding stylesheet

--- src/userScenes/Withdrawal/Withdrawal.jsx
+++ src/userScenes/Withdrawal/Withdrawal.jsx
@@ -1,15 +1,219 @@
-import React from 'react'
-import AdminHeader from '../userGlobal/AdminHeader'
-import { Box } from '@mui/material'
+import React, { useEffect, useState } from "react";
+import styles from "./withdrawal.module.css";
+import AdminHeader from "../userGlobal/AdminHeader";
+import {
+  setCurrentUser,
+  setRecipientDetails,
+  setTransferAccess,
+  setUserTokenAndEmail,
+} from "../../redux/userInfo/userInfoAction";
+import {
+  selectCurrentUser,
+  selectRecipientDetails,
+  selectTransferAccess,
+  selectUserTokenAndEmail,
+} from "../../redux/userInfo/userSelect";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import axios from "axios";
 
-const Withdrawal = () => {
+const Withdrawal = ({
+  userVerify,
+  transferDetails,
+  setTransferDetails,
+  setTransferAccess,
+  transferAccess,
+}) => {
+  const { token } = userVerify;
+
+  const [bankList, setBankList] = useState(null);
+  const [code, setCode] = useState("");
+  const [accNumber, setAccountNumber] = useState("");
+  const [accountName, setAccountName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [resMessage, setResMessage] = useState(null);
+  const [showName, setShowName] = useState(false);
+  const [showAmount, setShowAmount] = useState(false);
+
+  useEffect(() => {
+    const fetchBankList = async () => {
+      const url =
+        "https://mlm.zurupevarietiesstore.com/api/transfers/bank-list";
+
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      try {
+        const res = await axios.get(url, { headers });
+        const list = res.data.data.gateway_response;
+        setBankList(list);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    return () => fetchBankList();
+  }, [token]);
+
+  const resolveAccount = async (e) => {
+    e.preventDefault();
+    const url =
+      "https://mlm.zurupevarietiesstore.com/api/transfers/resolve-account";
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: "application/json",
+      },
+      params: { account_number: accNumber, bank_code: code },
+    };
+    try {
+      const res = await axios.get(url, config);
+      const details = res.data.data.gateway_response;
+      console.log(details);
+      setTransferDetails(details);
+      const transfer_access = {
+        recipient_code: res.data.data.recipient_code,
+        reference: res.data.data.reference,
+      };
+      setTransferAccess(transfer_access);
+      setAccountName(transferDetails.account_name);
+      setResMessage(res.data.message);
+      setShowName(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const createRecipientCode = async (e) => {
+    e.preventDefault();
+    const url = "https://mlm.zurupevarietiesstore.com/api/transfers/recipient";
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const body = {
+      account_number: transferDetails.account_number,
+      name: transferDetails.account_name,
+      bank_code: code,
+    };
+    try {
+      const res = await axios.post(url, body, config);
+      console.log(res.data);
+      setShowAmount(true);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const initiateTransfer = async (e) => {
+    e.preventDefault();
+    const url = "https://mlm.zurupevarietiesstore.com/api/transfers/initiate";
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const body = {
+      amount: amount,
+      recipient_code: transferAccess.recipient_code,
+      reference: transferAccess.reference,
+    };
+    try {
+      const res = await axios.post(url, body, config);
+      console.log(res.data)
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div>
-       <Box display="flex" justifyContent="space-between" alignItems="center">
-          <AdminHeader title="Withdrawal" subtitle="Welcome to your withdrawal section" />
-        </Box>
-    </div>
-  )
-}
+    <>
+      <AdminHeader
+        title="Withdrawal"
+        subtitle="Welcome to your withdrawal section"
+      />
+      <div className={styles.withdrawal}>
+        <div className={styles.container}>
+          <form onSubmit={initiateTransfer} className={styles.form}>
+            <div className={styles.formgroup}>
+              <label>Select your bank below</label>
+              <select onChange={(event) => setCode(event.target.value)}>
+                <option disabled>Select your bank:</option>
+                {bankList?.map((key) => (
+                  <option key={key.name} value={key.code}>
+                    {key.name}
+                  </option>
+                ))}
+              </select>
+            </div>
 
-export default Withdrawal
+            <div className={styles.formgroup}>
+              <label>Account Number:</label>
+              <div>
+                <input
+                  type="text"
+                  onChange={(e) => setAccountNumber(e.target.value)}
+                />
+                <span type="click" onClick={(e) => resolveAccount(e)}>
+                  confirm
+                </span>
+              </div>
+            </div>
+            {showName && (
+              <>
+                <div className={styles.formgroup}>
+                  <label>Account Name:</label>
+                  <input
+                    type="text"
+                    onChange={(e) => setAccountName(e.target.value)}
+                    value={accountName}
+                    disabled
+                  />
+                  <p className={styles.approved}>{resMessage}</p>
+                </div>
+                <div
+                  className={styles.createrequest}
+                  onClick={(e) => createRecipientCode(e)}
+                >
+                  Create Transfer Request
+                </div>
+              </>
+            )}
+            {showAmount && (
+              <>
+                <div className={styles.formgroup}>
+                  <label>Amount:</label>
+                  <input
+                    type="text"
+                    onChange={(e) => setAmount(e.target.value)}
+                    value={amount}
+                  />
+                </div>
+              </>
+            )}
+            {showAmount && (
+              <button className={styles.submitbutton} type="submit">
+                submit
+              </button>
+            )}
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  userData: selectCurrentUser,
+  userVerify: selectUserTokenAndEmail,
+  transferDetails: selectRecipientDetails,
+  transferAccess: selectTransferAccess,
+});
+const mapDispatchToProps = (dispatch) => ({
+  setUserVerify: (user) => dispatch(setUserTokenAndEmail(user)),
+  setUserData: (userData) => dispatch(setCurrentUser(userData)),
+  setTransferDetails: (details) => dispatch(setRecipientDetails(details)),
+  setTransferAccess: (details) => dispatch(setTransferAccess(details)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(Withdrawal);
