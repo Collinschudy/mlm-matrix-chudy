661e9d99a438a0d1c0f8e05037df18d7808271cc
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-08-14 16:03:22 +0100

    moved to the asset folder in the src directory

---
+++ src/userScenes/UserDashboardContent/DashContent.jsx
@@ -0,0 +1,256 @@
+import { Box } from "@mui/material";
+import React from "react";
+import styles from './dashcontent.module.css'
+
+import StatBox from "../../components/StatBoxUser";
+import AdminHeader from "../userGlobal/AdminHeader";
+
+import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
+import PaymentIcon from "@mui/icons-material/Payment";
+import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import ReceiptIcon from "@mui/icons-material/Receipt";
+import SendIcon from "@mui/icons-material/Send";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+
+// import SummarizeOutlinedIcon from "@mui/icons-material/SummarizeOutlined";
+// import MonetizationOnOutlinedIcon from "@mui/icons-material/MonetizationOnOutlined";
+// import KeyboardArrowDownOutlinedIcon from "@mui/icons-material/KeyboardArrowDownOutlined";
+// import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+// import RadioButtonCheckedOutlinedIcon from "@mui/icons-material/RadioButtonCheckedOutlined";
+// import GppBadIcon from "@mui/icons-material/GppBad";
+// import BlockIcon from "@mui/icons-material/Block";
+// import DownloadingIcon from "@mui/icons-material/Downloading";
+// import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
+// import PeopleIcon from "@mui/icons-material/People";
+// import EmailIcon from "@mui/icons-material/Email";
+// import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+// import TrafficIcon from "@mui/icons-material/Traffic";
+// import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+// import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { setToggleView } from "../../redux/navToggle/navToggleAction";
+import { selectToggleView } from "../../redux/navToggle/navToggleSelect";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { selectCurrentUser, selectPaymentResponse } from "../../redux/userInfo/userSelect";
+import { setCurrentUser } from "../../redux/userInfo/userInfoAction";
+
+const DashContent = ({ collapsed, setIsCollapsed, userData, setUserData, paymentResponse }) => {
+  const isNotMobile = useMediaQuery("(min-width: 600px)");
+  const usernameCap = userData?.username.charAt(0).toUpperCase() + userData?.username.slice(1)
+
+  return (
+    <>
+      <div className={styles.adminheaderbox}>
+        <AdminHeader title="Dashboard" subtitle={`Welcome to your dashboard, ${usernameCap} `} />
+      </div>
+      <div>
+        <Box
+          m="20px"
+          backgroundColor="transparent"
+          borderRadius="5px"
+          p={!isNotMobile ? "0" : "20px"}
+        >
+          <Box
+            display="grid"
+            gridTemplateColumns="repeat(12, 1fr)"
+            gridAutoRows="180px"
+            gap="20px"
+            sx={{
+              "& > div": { gridColumn: isNotMobile ? undefined : "span 12" },
+            }}
+          >
+            <Box
+              gridColumn="span 4"
+              background={``}
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title={`\u20A6${paymentResponse ? Number(paymentResponse.amount) + Number(userData.balance) : '0'}`}
+                subtitle="Current Balance"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+
+                // '#8ec33e'
+                // "linear-gradient(to bottom right, rgba(0,0,50), yellowgreen)",
+              }}
+            >
+              <StatBox
+                icon={<LocalAtmIcon sx={{ height: "4em", width: "4em" }} />}
+                title='0'
+                subtitle="Total Deposits"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<PaymentIcon sx={{ height: "4em", width: "4em" }} />}
+                title={"\u20A6"}
+                subtitle="Total Withdrawal"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title={`\u20A6`}
+                subtitle="Total Referral Commission"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <MonetizationOnIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title="0"
+                subtitle="Total Level Commission"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<ReceiptIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total E-Pin Recharged"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={
+                  <AddCircleOutlinedIcon sx={{ height: "4em", width: "4em" }} />
+                }
+                title="0"
+                subtitle="Total E-Pin Generated"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<SendIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total Transferred Balance"
+              />
+            </Box>
+            <Box
+              gridColumn="span 4"
+              backgroundColor="grey"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              borderRadius="8px"
+              sx={{
+                background:
+                  "linear-gradient(to bottom right, #14213d, #8ec33e)",
+              }}
+            >
+              <StatBox
+                icon={<PersonAddIcon sx={{ height: "4em", width: "4em" }} />}
+                title="0"
+                subtitle="Total Direct Referrals"
+              />
+            </Box>
+          </Box>
+        </Box>
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collapsed: selectToggleView,
+  userData: selectCurrentUser,
+  paymentResponse: selectPaymentResponse
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setIsCollapsed: () => dispatch(setToggleView()),
+  setUserData: (userData) => dispatch(setCurrentUser(userData)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashContent);
---
+++ src/userScenes/userGlobal/Searchbar/Searchbar.jsx
@@ -0,0 +1,73 @@
+import { IconButton, useMediaQuery } from "@mui/material";
+import styles from './searchbar.module.css';
+import SearchIcon from "@mui/icons-material/Search";
+import MenuIcon from "@mui/icons-material/Menu";
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
+import { connect } from "react-redux";
+import { selectToggleView } from "../../../redux/navToggle/navToggleSelect";
+import { selectMobileView } from "../../../redux/mobileToggle/mobileToggleSelect";
+import { setMobileView } from "../../../redux/mobileToggle/mobileToggleAction";
+import { setToggleView } from "../../../redux/navToggle/navToggleAction";
+import { createStructuredSelector } from "reselect";
+
+const Searchbar = ({ collapsed, setIsCollapsed, show, setShow }) => {
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode);
+  const isNotMobile = useMediaQuery("(min-width:600px)");
+
+  return (
+    <>
+      <div className={styles.container}>
+        {collapsed && show ? (
+          <IconButton
+            type="button"
+            sx={{ color: "lightgrey" }}
+            onClick={() => {
+              isNotMobile && setIsCollapsed((prev) => !prev);
+              !isNotMobile && setShow((prev) => !prev);
+            }}
+          >
+            <CancelOutlinedIcon />
+          </IconButton>
+        ) : (
+          <IconButton
+            type="button"
+            sx={{ color: "lightgrey" }}
+            onClick={() => {
+              isNotMobile && setIsCollapsed((prev) => !prev);
+              !isNotMobile && setShow((prev) => !prev);
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+        )}
+
+        {/* <Box
+          display="flex"
+          borderRadius="1.5em"
+          justifyContent="space-between"
+          backgroundColor="white"
+          width="100%"
+          maxWidth={isNotMobile ? "30%" : "70%"}
+        >
+          <InputBase sx={{ ml: 2 }} placeholder="Search" />
+          <IconButton type="button">
+            <SearchIcon sx={{ color: colors.grey[500] }} />
+          </IconButton>
+        </Box> */}
+      </div>
+    </>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collapsed: selectToggleView,
+  show: selectMobileView,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setIsCollapsed: () => dispatch(setToggleView()),
+  setShow: () => dispatch(setMobileView()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Searchbar);
