4f7c389d8392adc282a0bf4732e1a51ea7576775
Collinschudy <collinschudy@gmail.com>
2023-10-30 20:06:15 +0100

    created the CallToAction component

---
+++ src/components/CallToAction/Calltoaction.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './calltoaction.module.css'
+
+const Calltoaction = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.inner}>
+            Join us Today!
+            <button>
+                Click to Get Started
+            </button>
+        </div>
+    </div>
+  )
+}
+
+export default Calltoaction
