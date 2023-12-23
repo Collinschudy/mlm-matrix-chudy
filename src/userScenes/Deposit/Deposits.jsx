990143e2068f86dfa7f0b78619745ebfd4fc6ea7
Collinschudy <collinschudy@gmail.com>
2023-12-23 22:42:54 +0100

    changed the api call url and made the neccessary adjustments

--- src/userScenes/Deposit/Deposits.jsx
+++ src/userScenes/Deposit/Deposits.jsx
@@ -19,41 +19,64 @@ import {
 import { createStructuredSelector } from "reselect";
 
 const Deposits = ({ userData, userVerify }) => {
-  const [txn_ref, setTxn_ref] = useState("");
-
-  const generateRef = () => {
-    const timestamp = Date.now();
-    const randStr = uuidv4();
-    let reference = randStr;
-    return reference;
-  };
-
-  const initiateTransaction = async (e) => {
+  const [paymentUrl, setPaymentUrl] = useState("");
+  const makePayment = async (e) => {
     e.preventDefault();
     const { token } = userVerify;
-
-    try {
-      const reference = generateRef();
-      setTxn_ref(reference);
-      const url =
-        "https://mlm.zurupevarietiesstore.com/api/transactions/initialize";
-      const body = {
-        email: userData.email,
-        amount: 1000,
-        callback_url: "http://localhost:3000/verifypayment",
-        phone: userData.phone_number,
-        reference: reference,
-      };
-      const headers = {
+    const url = "https://mlm.a1exchange.net/api/v1/payment/pay";
+    const config = {
+      headers: {
         Authorization: `Bearer ${token}`,
-      };
-      const res = await axios.post(url, body, { headers });
-      const authorizationUrl = res.data.data.transaction.authorization_url;
-      window.location.href = authorizationUrl;
+      },
+    };
+    const body = {
+      amount: "1000",
+      type: "membership",
+    };
+    try {
+      const res = await axios.post(url, body, config);
+      console.log(res);
+      setPaymentUrl(res.data.data.payment_url);
+      alert(res.data.message);
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   };
+  // const [txn_ref, setTxn_ref] = useState("");
+
+  // const generateRef = () => {
+  //   const timestamp = Date.now();
+  //   const randStr = uuidv4();
+  //   let reference = randStr;
+  //   return reference;
+  // };
+
+  // const initiateTransaction = async (e) => {
+  //   e.preventDefault();
+  //   const { token } = userVerify;
+
+  //   try {
+  //     const reference = generateRef();
+  //     setTxn_ref(reference);
+  //     const url =
+  //       "https://mlm.zurupevarietiesstore.com/api/transactions/initialize";
+  //     const body = {
+  //       email: userData.email,
+  //       amount: 1000,
+  //       callback_url: "http://localhost:3000/verifypayment",
+  //       phone: userData.phone_number,
+  //       reference: reference,
+  //     };
+  //     const headers = {
+  //       Authorization: `Bearer ${token}`,
+  //     };
+  //     const res = await axios.post(url, body, { headers });
+  //     const authorizationUrl = res.data.data.transaction.authorization_url;
+  //     window.location.href = authorizationUrl;
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <>
       <AdminHeader
@@ -63,20 +86,29 @@ const Deposits = ({ userData, userVerify }) => {
       <div className={styles.deposits}>
         <div className={styles.depositbox}>
           <h3>Begin your payment process</h3>
-          <p className={styles.warning}>*Note* Your details are autofilled, just click on the deposit button below</p>
-          <form onSubmit={initiateTransaction} className={styles.form}>
-            <div className={styles.formgroup}>
+          <p className={styles.warning}>
+            *Note* Details are autofilled, just click on the deposit button
+            below
+          </p>{
+            userData.member_status === "active" ? (
+              <>
+              <p>Hey, {userData.username},</p>
+              <p>You have already made your one time payment!</p>
+              </>
+            ) : (
+              <form onSubmit={makePayment} className={styles.form}>
+            {/* <div className={styles.formgroup}>
               <label>Email</label>
               <input type="text" defaultValue={userData.email} disabled />
-            </div>
-            <div className={styles.formgroup}>
+            </div> */}
+            {/* <div className={styles.formgroup}>
               <label>Phone</label>
               <input
                 type="text"
                 defaultValue={userData.phone_number}
                 disabled
               />
-            </div>
+            </div> */}
 
             <div className={styles.formgroup}>
               <label>Amount</label>
@@ -84,8 +116,8 @@ const Deposits = ({ userData, userVerify }) => {
             </div>
 
             <div className={styles.formgroup}>
-              <label>Reference</label>
-              <input type="text" defaultValue={txn_ref && txn_ref} disabled />
+              <label>Type</label>
+              <input type="text" defaultValue={"membership"} disabled />
             </div>
             <div>
               <button className={styles.button} type="submit">
@@ -93,6 +125,21 @@ const Deposits = ({ userData, userVerify }) => {
               </button>
             </div>
           </form>
+            )
+          }
+          
+          {(paymentUrl &&
+            <div className={styles.buttonContainer}>
+              <button
+                onClick={() => {
+                  window.location.href = paymentUrl;
+                }}
+                className={styles.paymentButton}
+              >
+                Make Payment
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
