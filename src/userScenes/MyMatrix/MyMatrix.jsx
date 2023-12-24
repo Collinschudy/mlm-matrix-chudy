ea4726343e67a7eda150e66903aba8fa2855efd9
Collinschudy <collinschudy@gmail.com>
2023-12-24 12:41:54 +0100

    created MyMatrix css file

--- src/userScenes/MyMatrix/MyMatrix.jsx
+++ src/userScenes/MyMatrix/MyMatrix.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from './mymatrix.module.css'
 import AdminHeader from '../userGlobal/AdminHeader'
+import { Tree, TreeNode } from 'react-organizational-chart';
+
+
 
 const MyMatrix = () => {
   return (
     <>
     <AdminHeader title='My Matrix' subtitle='Welcome to your matrix section' />
-    <div>MyMatrix</div>
+    <div className={styles.matrixSection}>
+    <Tree label={<div>Root</div>}>
+    <TreeNode label={<div>Child 1</div>}>
+      <TreeNode label={<div>Grand Child</div>} />
+    </TreeNode>
+  </Tree>
+    </div>
     </>
   )
 }
