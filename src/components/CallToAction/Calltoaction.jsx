ca288a2df06d63d6f31df1105ed3ce2c8ff712c5
Collinschudy <collinschudy@gmail.com>
2024-01-13 06:40:17 +0100

    added the useNavigate hook to the sign up function

--- src/components/CallToAction/Calltoaction.jsx
+++ src/components/CallToAction/Calltoaction.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './calltoaction.module.css'
+import { useNavigate } from 'react-router-dom';
 
 const Calltoaction = () => {
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
         <div className={styles.inner}>
             <h3 className={styles.heading}>Join us today!</h3>
-            <button>
+            <button onClick={() => navigate("/signup")}>
                 Click to Get Started
             </button>
         </div>
